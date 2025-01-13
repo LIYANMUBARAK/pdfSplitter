e8548462a49609d8007625d83332f1547626c879
LIYANMUBARAK <115862601+LIYANMUBARAK@users.noreply.github.com>
2025-01-13 23:50:36 +0530

    added empty string policy handle v3

--- src/controllers/pdfController.ts
+++ src/controllers/pdfController.ts
@@ -489,7 +489,8 @@ export async function sendEmailWebhook(req:Request,res:Response){
         missingFields.push("claim_number_not_found(webhook)")
       }
       
-        if(!policy || policy === "") {
+        if (!policy || isNaN(Number(policy)) || policy.trim() === "") {
+          console.log("entered not policy oresent condition")
         // const tag = "policy_number_not_found(webhook)"
         // await createTagsForContact(tag_contact_id, tag, locationId )
         // missingFields.push("policy_number_not_found(webhook)")
